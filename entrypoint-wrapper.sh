d62179afe4e80ed91e876fb0002c6e37bc233e57
Jeesmon Jacob <jjacob@us.ibm.com>
2021-04-26 15:29:55 -0400

    Enable gzip compression in nginx
    Signed-off-by: Jeesmon Jacob <jjacob@us.ibm.com>

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ -n "$APP_CONFIG" ]
 then
     cp $APP_CONFIG /usr/share/nginx/html/app-config.js
+fi
+
+if [ -n "$NGINX_CONFIG" ]
+then
+    cp $NGINX_CONFIG /etc/nginx/conf.d/default.conf
 fi
 
 exec /usr/src/entrypoint.sh "$@"
